82d5458d30063fd11628fd8a9d3a148a706971d0
NaoyaTanikawa <34472406+NaoyaTanikawa@users.noreply.github.com>
2017-12-19 09:52:04 +0900

    Update test.js

--- test.js
+++ test.js
@@ -23,7 +23,6 @@ test('responds to requests', (t) => {
       // Assert content checks
       t.notEqual(body.indexOf("<title>Node.js Getting Started on Heroku</title>"), -1);
       t.notEqual(body.indexOf("Getting Started with Node on Heroku"), -1);
-      console.log(body);
     });
   });
 });
