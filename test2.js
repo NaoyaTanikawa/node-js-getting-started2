bbd05a776c0181be681a7015315477025576682d
NaoyaTanikawa <34472406+NaoyaTanikawa@users.noreply.github.com>
2017-12-19 09:48:14 +0900

    Update test2.js

--- test2.js
+++ test2.js
@@ -7,12 +7,12 @@ const env = Object.assign({}, process.env, {PORT: 5000});
 const child = spawn('node', ['index.js'], {env});
 
 test('responds to requests', (t) => {
-  t.plan(4);
+  t.plan(2);
   
   // Wait until the server is ready
   child.stdout.on('data', _ => {
     // Make a request to our app
-    request('http://127.0.0.1:5000', (error, response, body) => {
+    request('http://127.0.0.1/cool:5000', (error, response, body) => {
       // stop the server
       child.kill();
       
@@ -21,8 +21,6 @@ test('responds to requests', (t) => {
       // Successful response
       t.equal(response.statusCode, 200);
       // Assert content checks
-      t.notEqual(body.indexOf("<title>Node.js Getting Started on Heroku</title>"), -1);
-      t.notEqual(body.indexOf("Getting Started with Node on Heroku"), -1);
       console.log(body);
     });
   });
